42fc8ddabe0fe1429ee916fe37739981e4830426
OliviervDuuren <46646867+OliviervDuuren@users.noreply.github.com>
2019-01-24 09:57:03 +0100

    Final prototype music system
    This is the digital prototype for a master project at the TU/e

---
+++ db.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$host = "localhost";
+$user = "u483017291_dm1";
+$password = "d18vStFOSWmJ";
+$db = "u483017291_music";
+
+    $connect = new mysqli($host, $user, $password, $db);
+   
+?>
